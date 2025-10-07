3143ea07fa677f4be4b90aa8fcbf640f8fddf79a
Yan Sun <roza4x@gmail.com>
2025-10-07 20:13:00 +0300

    Создаем функция загрузки ссылки на изображение

--- Dogs.py
+++ Dogs.py
@@ -14,6 +14,7 @@ def get_random_dog_image():
         mb.showerror("Ошибка", f"Ошибка при запросе к API: {e}")
         return None
 
+
 def show_image():
     image_url = get_random_dog_image()
     if image_url:
